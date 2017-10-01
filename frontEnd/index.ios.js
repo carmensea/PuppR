5468f7499a28fb32e20f48d55bfd579f3672038a
Carmen Cojocariu <cojocariucarmen@gmail.com>
2017-09-30 20:31:28 -0700

    Fix merge issues

--- frontEnd/index.ios.js
+++ frontEnd/index.ios.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
 import ShowDogList from './src/components/ShowDogList';
 import CarouselExample from './src/components/ShowCarousel';
