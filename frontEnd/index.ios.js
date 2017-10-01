7031f5e01d3c3263cd43047158594860bf850f0c
Carmen Cojocariu <cojocariucarmen@gmail.com>
2017-09-30 18:48:43 -0700

    Add axios call to local host

--- frontEnd/index.ios.js
+++ frontEnd/index.ios.js
@@ -1,53 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/Axios';
+import DogList from './src/components/DogList';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+const App = () => (
+  <View>
+    <Header />
+    <DogList />
+  </View>
+);
 
-export default class frontEnd extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('frontEnd', () => frontEnd);
+AppRegistry.registerComponent('frontEnd', () => App);
