1e704dc3ab655c8a2c41dfc6831ebfa79ab143fb
Marissa Tomko <marissatomko@gmail.com>
2017-10-02 14:19:10 -0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,27 +12,3 @@ User.create(name: "Sara")
 User.create(name: "Marissa")
 User.create(name: "Vivian")
 
-Shelter.create(phone: "123-456-7890", address: "123 Main Street, San Francisco, CA")
-Shelter.create(phone: "234-567-8901", address: "234 Main Street, San Francisco, CA")
-Shelter.create(phone: "345-678-9012", address: "345 Main Street, San Francisco, CA")
-Shelter.create(phone: "456-789-0123", address: "456 Main Street, San Francisco, CA")
-Shelter.create(phone: "567-890-1234", address: "567 Main Street, San Francisco, CA")
-
-Dog.create(name: "Fido", size: "S", age: "Young", gender: "M", shelter_id: 1)
-Dog.create(name: "Fluffy", size: "M", age: "Adult", gender: "F", shelter_id: 2)
-Dog.create(name: "Spot", size: "L", age: "Senior", gender: "M", shelter_id: 3)
-Dog.create(name: "Clyde", size: "S", age: "Baby", gender: "F", shelter_id: 4)
-Dog.create(name: "Blackie", size: "M", age: "Young", gender: "F", shelter_id: 5)
-
-Favorite.create(user_id: 1, dog_id: 1)
-Favorite.create(user_id: 2, dog_id: 2)
-Favorite.create(user_id: 3, dog_id: 3)
-Favorite.create(user_id: 4, dog_id: 4)
-Favorite.create(user_id: 5, dog_id: 5)
-
-
-
-
-
-
-
