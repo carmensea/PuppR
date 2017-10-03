4fe00c1c9dd81707c6d204fd9ce24f2595ea349c
Sara Bartell <saracbartell@gmail.com>
2017-10-03 11:22:13 -0700

    Create a tab.js file and tabs on homepage

---
+++ frontEnd/src/components/TabIcon.js
@@ -0,0 +1,13 @@
+import React, { Component }  from 'react';
+import { View, Text } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+class TabIcon extends Component {
+render(){
+  return (
+    <View>
+      <Text></Text>
+    </View>
+    );
+  }
+};
+export default TabIcon;
