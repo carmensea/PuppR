6b4e70c7b9ce7b36ca8f33ab2b33e696dcb3aaa6
Carmen Cojocariu <cojocariucarmen@gmail.com>
2017-10-03 10:23:25 -0700

    Add shelter name column to database, update method to also add shelter name when creating shelter/dog

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002171329) do
+ActiveRecord::Schema.define(version: 20171003171117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20171002171329) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "shelter_id"
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
