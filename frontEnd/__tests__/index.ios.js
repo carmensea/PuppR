f038398fb59d067d3e05c5a11c1062a057a4e4a9
Scott M Fulton <scottmfulton@Scotts-MacBook-Pro.local>
2017-10-04 16:01:28 -0700

    write more Rspec tests

--- frontEnd/__tests__/index.ios.js
+++ frontEnd/__tests__/index.ios.js
@@ -1,12 +1,13 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.ios.js';
+import Form from '../index.ios';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
+test('renders correctly', () => {
   const tree = renderer.create(
-    <Index />
-  );
+    <Form />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
