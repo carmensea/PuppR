2f592ad55087c8d121b23d81080950181aa02a7d
Scott M Fulton <scottmfulton@Scotts-MacBook-Pro.local>
2017-10-05 17:37:21 -0700

    write more rspec

--- spec/controllers/dogs_controller_spec.rb
+++ spec/controllers/dogs_controller_spec.rb
@@ -6,12 +6,12 @@ let!(:shelter) { Shelter.create(name: "Rainbow Bridge Dog Shelter", address: "12
 let!(:dog) { Dog.create(size: "L", name: "Fido", age: "Young", photo: "http://www.google.com", description: "Coolest dog ever", sex: "M", shelter_id: shelter.id, id: 1)}
 let!(:favorite) { Favorite.create!(id: 1, user_id: user.id, dog_id: dog.id)}
 
-# describe "GET #index" do
-#   it "responds with status code 200" do
-#     get :index
-#     expect(response).to have_http_status 200
-#   end    
-# end
+describe "GET #index" do
+  it "responds with status code 200" do
+    get :index, params: { id: user.id }
+    expect(response).to have_http_status 200
+  end    
+end
 
 describe "GET #show" do
   it "responds with status code 200" do
@@ -40,15 +40,7 @@ end
 #     expect { delete(:destroy) }.to change(user.favorites, :count).by(-1)
 #   end
 # end
-    # it "sets a notice that the game was destroyed" do
-    #   delete :destroy, { id: game.id }
-    #   expect(flash[:notice]).to match /Game was successfully destroyed/
-    # end
-
-    # it "redirects to the games list" do
-    #   delete :destroy, { id: game.id }
-    #   expect(response).to redirect_to games_url
-    # end
+    
 	
 
 
@@ -57,22 +49,7 @@ end
 
 
 
-# describe "GET #show" do
-#   it "responds with status code 200" do
-#     get :show, {dog: :dog, shelter: :shelter} 
-#     expect(response).to have_http_status 200
-#   end
 
-#   it "assigns the correct dog as @dog" do
-#     get :show, {dog: dog, shelter: shelter} 
-#     expect(assigns(:dog)).to eq(dog)
-#   end
-
-#   it "renders the :show template" do
-#     get :show, {dog: dog, shelter: shelter} 
-#     expect(response).to render json: dog_shelter_response
-#   end
-# end
 
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+	let!(:user_params) { {user: { name: "Scott", email: "scott@scott.com", password: "password", password_confirmation: "password"}}}
+	let!(:bad_params) { {user: { name: "Fulton", profanity: "Hot diggity dog!", password: "password", password_confirmation: "password"}}}
 
+	describe "POST #create" do 
+	  it "responds with status code 200" do
+	    post :create, params: user_params, format: :json
+	    expect(response).to have_http_status 200
+	    expect(User.last.name).to eq('Scott')
+	  end    
+	
+	  it "won't create a user if bad params are passed" do
+	    post :create, params: bad_params, format: :json
+	    expect(response).to have_http_status 422
+	  end 
+	end
 end
